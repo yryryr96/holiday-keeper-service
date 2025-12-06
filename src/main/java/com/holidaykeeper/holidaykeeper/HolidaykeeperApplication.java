8595d5d684addd4fd9d51de76dfd5a5481174034
정영록 <s9285s@naver.com>
2025-12-06 18:51:54 +0900

    init: init holiday-keeper-project

---
+++ src/main/java/com/holidaykeeper/holidaykeeper/HolidaykeeperApplication.java
@@ -0,0 +1,13 @@
+package com.holidaykeeper.holidaykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaykeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaykeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holidaykeeper/holidaykeeper/HolidaykeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holidaykeeper.holidaykeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaykeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
