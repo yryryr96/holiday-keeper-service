493b6c39762625b9452f58cb0fc3303019afa8ce
정영록 <s9285s@naver.com>
2025-12-07 23:07:24 +0900

    feat: country controller swagger 설명 작성

--- src/main/java/com/holidaykeeper/holidaykeeper/controller/CountryController.java
+++ src/main/java/com/holidaykeeper/holidaykeeper/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.holidaykeeper.holidaykeeper.controller;
 import com.holidaykeeper.holidaykeeper.dto.response.ApiResponse;
 import com.holidaykeeper.holidaykeeper.dto.response.CountryListResponse;
 import com.holidaykeeper.holidaykeeper.service.CountryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -14,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/countries")
-@Validated
 public class CountryController {
 
     private final CountryService  countryService;
 
+    @Operation(summary = "지원 국가 조회", description = "지원하는 국가 이름, 국가 코드를 조회합니다.")
     @GetMapping
     public ApiResponse<CountryListResponse> getCountries() {
         return ApiResponse.ok(countryService.getCountries());
